7ce2508ebe50c59e3e8b7304c3526ecad2653ba8
yassan-s <yasuro.2103@gmail.com>
2022-06-21 22:11:35 +0900

    first commit

---
+++ src/main/java/com/tryfeaturelist/TryFeatureListApplication.java
@@ -0,0 +1,13 @@
+package com.tryfeaturelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryFeatureListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryFeatureListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryfeaturelist/TryFeatureListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryfeaturelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryFeatureListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
